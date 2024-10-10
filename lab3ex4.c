e98d3cd98a9676bd244dfbb3bc31bc11afe0e71e
nuray mammadli <nmammadl20381@ada.edu.az>
2024-10-10 15:49:48 +0400

    Your commit message

--- lab3ex4.c
+++ lab3ex4.c
@@ -2,24 +2,6 @@
 
 int main() {
     int n;
-<<<<<<< HEAD
-    float num, sum = 0.0;
-
-    printf("Enter the number of floats (n): ");
-    scanf("%d", &n);
-
-    while (n > 0) {
-        printf("Enter a float: ");
-        scanf("%f", &num);
-        sum += num;
-        n--;
-    }
-
-    printf("Sum of the floats: %.1f\n", sum);
-
-    return 0;
-}
-=======
     float sum = 0.0f, number;
 
     scanf("%d", &n);
@@ -33,4 +15,3 @@ int main() {
 
     return 0;
 }
->>>>>>> 831c033 (Initial commit)
